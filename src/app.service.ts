a62a715fffe22109375e15b51510adf4620ceb63
HermannMarx <marxhermann@web.de>
2022-10-13 13:19:09 +0200

    last test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'I luv coffee 4ever!!!';
+    return 'Welcome to the best coffee storage!';
   }
 }
